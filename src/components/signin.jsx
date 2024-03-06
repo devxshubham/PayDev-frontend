631e6e079c9e7d3833e64ef707c2002dfd7f8749
devxshubham <devshubhamcode@gmail.com>
2024-03-07 00:16:50 +0530

    added signin page

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,3 +1,26 @@
 export const Signin = ()=>{
-    
+    return <div className="flex bg-gray-500 justify-center items-center h-[100dvh]">
+        <div className="flex flex-col gap-5 bg-white rounded-lg border p-5 max-w-[300px]">
+            <div className="flex flex-col items-center">
+                <h3 className="font-bold text-[30px]">Sign Up</h3>
+                <p className="text-center text-gray-600">Enter your credentials to access your account</p>
+            </div>
+            <div className="flex flex-col gap-2">
+                <div className="flex flex-col text-[#808080]  gap-1">
+                    <h2 className="font-semibold text-black">Email</h2>
+                    <input className="border bg-transparent outline-none rounded py-1 px-2" type="text" placeholder="abc@gmail.com"/>
+                </div>
+                <div className="flex flex-col text-[#808080] gap-1">
+                    <h2 className="font-semibold text-black">Password</h2>
+                    <input className="border outline-none rounded py-1 px-2" type="password" placeholder=""/>
+                </div>
+                
+            </div>
+            <div className="flex flex-col items-center gap-2">
+                <button className="py-1 w-full rounded bg-black text-white">Sign In</button>
+                <p className="font-[500]">Don't have an account?  <button
+                    className="underline">Sign Up</button></p>
+            </div>
+        </div>
+    </div>
 }
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,30 +1,31 @@
 export const Signup = ()=>{
-    return <div className="border p-5 max-w-[300px]">
+    return <div className="flex flex-col gap-5 border p-5 max-w-[300px]">
         <div className="flex flex-col items-center">
             <h3 className="font-bold text-[30px]">Sign Up</h3>
             <p className="text-center text-gray-600">Enter your information to create an account</p>
         </div>
-        <div>
-            <div className="flex flex-col">
-                <h2 className="font-semibold">First Name</h2>
-                <input type="text" placeholder="Shubham"/>
+        <div className="flex flex-col gap-2">
+            <div className="flex flex-col text-[#808080]  gap-1">
+                <h2 className="font-semibold text-black">First Name</h2>
+                <input className="border bg-transparent outline-none rounded py-1 px-2" type="text" placeholder="Shubham"/>
             </div>
-            <div>
-                <h2>Last Name</h2>
-                <input type="text" placeholder="Dev"/>
+            <div className="flex flex-col text-[#808080] gap-1">
+                <h2 className="font-semibold text-black">Last Name</h2>
+                <input className="border outline-none rounded py-1 px-2" type="text" placeholder="Dev"/>
             </div>
-            <div>
-                <h2>Email</h2>
-                <input type="text" placeholder="abc@gmail.com"/>
+            <div className="flex flex-col text-[#808080] gap-1">
+                <h2 className="font-semibold text-black">Email</h2>
+                <input className="border outline-none rounded py-1 px-2" type="text" placeholder="abc@gmail.com"/>
             </div>
-            <div>
-                <h2>Password</h2>
-                <input type="password" placeholder=""/>
+            <div className="flex flex-col text-[#808080] gap-1">
+                <h2 className="font-semibold text-black">Password</h2>
+                <input className="border outline-none rounded py-1 px-2" type="password" placeholder=""/>
             </div>
         </div>
-        <div>
-            <button>Sign Up</button>
-            <p>Already have an account? <a href="/">Login</a></p>
+        <div className="flex flex-col items-center gap-2">
+            <button className="py-1 w-full rounded bg-black text-white">Sign Up</button>
+            <p className="font-[500]">Already have an account?  <button
+                className="underline">Login</button></p>
         </div>
     </div>
 }
